70f2ac7dec3ae1278ae688d7b60dc6b695824e70
Cris2725 <cristopherquincer@gmail.com>
2023-07-17 10:36:29 -0600

    first commit

---
+++ Calculadora/main.py
@@ -0,0 +1,15 @@
+import tkinter as tk
+
+# Crear la ventana principal
+ventana = tk.Tk()
+
+# Configurar propiedades de la ventana
+ventana.title("Mi ventana")
+ventana.geometry("400x300")
+
+# Agregar un widget de etiqueta
+etiqueta = tk.Label(ventana, text="Hola, mundo!")
+etiqueta.pack()
+
+# Ejecutar el bucle principal de la ventana
+ventana.mainloop()
